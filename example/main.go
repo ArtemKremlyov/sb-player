e63aae992909d5d33804b963203fd1ac99dcf14d
ArtemKremlyov <gfvhhhggffghh@gmail.com>
2023-03-10 02:56:14 +0700

    [UPD] change example

--- example/main.go
+++ example/main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"time"
+
 	"github.com/ArtemKremlyov/player"
 )
 
 func main() {
-	mPlayer := player.NewPlayer()
-	mPlayer.AddSong(player.Song{
-		Duration: 10,
-		Name:     "test",
-	})
+	// создание плейлиста
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	err := mPlayer.Play()
+	p := player.New(ctx, cancel)
 
-	if err != nil {
-		fmt.Errorf("play: %v", err)
+	// добавить песни в плейлист
+	p.AddSong(player.Song{Name: "Song 1", Duration: 5 * time.Second})
+	p.AddSong(player.Song{Name: "Song 2", Duration: 4 * time.Second})
+	p.AddSong(player.Song{Name: "Song 3", Duration: 7 * time.Second})
+
+	if err := p.Play(); err != nil {
+		log.Fatalf("error while playing playlist: %v", err)
 	}
+
+	time.Sleep(3 * time.Second)
+
+	p.Pause()
+	time.Sleep(10 * time.Second)
+	if err := p.Play(); err != nil {
+		log.Println(err)
+	}
+	time.Sleep(10 * time.Second)
+	if err := p.Play(); err != nil {
+		log.Println(err)
+	}
+
+	time.Sleep(40 * time.Second)
 }
